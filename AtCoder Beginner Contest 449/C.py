acc52a7c1c3f83fd5f274c77ec4f2d5e5fb6366d
hirame6548 <hirame.6548@gmail.com>
2026-04-12 01:35:11 +0900

    add comment

--- AtCoder Beginner Contest 449/C.py
+++ AtCoder Beginner Contest 449/C.py
@@ -27,3 +27,16 @@ for i in range(N):
         counter += k_dict[S[i]]
     #print(d, counter)
 print(counter)
+
+
+"""
+
+辞書にない key を選んでも、0 を返してくれる
+
+from collections import defaultdict
+
+# デフォルト値 0 を返す辞書を使用
+window_counts = defaultdict(int)
+
+
+"""
