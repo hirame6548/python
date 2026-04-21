a7974c78cf32cf87ed54928b6978ac36d0158abe
hirame6548 <hirame.6548@gmail.com>
2026-04-22 01:35:48 +0900

    added test_main.py and replaced some file.

---
+++ AtCoder Beginner Contest 454/D/test_main.py
@@ -0,0 +1,50 @@
+import pytest
+import io
+from main import main
+
+@pytest.mark.parametrize("input_data, expected_output", [
+    (
+        # 入力
+        """6
+(xx)x
+x(xx)
+(x)x
+(xx)
+)x()x(
+)x()x(
+x
+(x)
+(((((xx)))))x
+x((((((((((xx))))))))))
+((xx)xx)xx
+(x((xx))x)(xx)
+""",
+        
+        # 期待される出力
+        """Yes
+No
+Yes
+No
+Yes
+Yes
+"""
+    ),
+
+
+
+    (
+        # 入力
+        """""",
+
+        # 期待される出力
+        """"""
+    ),
+])
+
+def test_main(monkeypatch, capsys, input_data, expected_output):
+    monkeypatch.setattr('sys.stdin', io.StringIO(input_data))
+    main()
+    captured = capsys.readouterr()
+
+    # 両方の末尾の改行や空白を削ぎ落としてから比較する
+    assert captured.out.strip() == expected_output.strip()
