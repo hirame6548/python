ea88cf71008634709c0f991809969c9be46dcd3e
hirame6548 <hirame.6548@gmail.com>
2026-04-22 02:13:47 +0900

    updating file constitute to validate test.

--- AtCoder Beginner Contest 454/C/test_main.py
+++ AtCoder Beginner Contest 454/C/test_main.py
@@ -1,32 +1,20 @@
 import pytest
 import io
-from D.main import main
+from C.main import main
 
 @pytest.mark.parametrize("input_data, expected_output", [
     (
         # 入力
-        """6
-(xx)x
-x(xx)
-(x)x
-(xx)
-)x()x(
-)x()x(
-x
-(x)
-(((((xx)))))x
-x((((((((((xx))))))))))
-((xx)xx)xx
-(x((xx))x)(xx)
+        """5 5
+1 2
+2 3
+3 4
+2 4
+5 2
 """,
         
         # 期待される出力
-        """Yes
-No
-Yes
-No
-Yes
-Yes
+        """4
 """
     ),
 
@@ -34,11 +22,37 @@ Yes
 
     (
         # 入力
-        """""",
+        """3 2
+2 1
+3 2
+""",
 
         # 期待される出力
-        """"""
+        """1
+"""
     ),
+
+
+
+    (
+        # 入力
+        """7 8
+2 6
+2 5
+3 6
+1 6
+1 2
+5 6
+2 3
+3 7
+""",
+
+        # 期待される出力
+        """6
+"""
+    ),
+
+
 ])
 
 def test_main(monkeypatch, capsys, input_data, expected_output):
