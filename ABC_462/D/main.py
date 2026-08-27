09a1b870cd3c6d7aa964347ab28485a7fe0ec9b4
hirame6548 <hirame.6548@gmail.com>
2026-08-27 18:30:57 +0900

    solved abc462

--- ABC_462/D/main.py
+++ ABC_462/D/main.py
@@ -3,6 +3,7 @@ from collections import deque, Counter, defaultdict
 from itertools import permutations, combinations, accumulate
 from bisect import bisect_left, bisect_right
 import heapq
+import math
 
 sys.setrecursionlimit(10**6)
 
@@ -14,9 +15,30 @@ def main():
 
     N = next(it)
     D = next(it)
-    st = [[next(it), next(it)] for _ in range(N)]
+    time = [0 for _ in range(10**6 + 1)]
 
-    print(N)
+    for i in range(N):
+        s = next(it)
+        t = next(it) - D + 1
+        if s < t:
+            time[s] += 1
+            time[t] -= 1
+
+    ans = 0
+    comb = [0, 0]
+    comb_c = 1
+    v = 0
+    for i in range(10**6 + 1):
+        v += time[i]
+        while comb_c < v:
+            comb_c += 1            
+            comb.append(math.comb(comb_c, 2))
+        ans += comb[v]
+
+    print(ans)
+
+
+        
 
 
 if __name__ == '__main__':
